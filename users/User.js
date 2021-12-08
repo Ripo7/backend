e20f773ced32f6afbf14f3689d661d455372370e
Doinsport <doinsport@MacBook-Pro.local>
2021-12-08 15:09:15 +0100

    frfrfrf

--- users/User.js
+++ users/User.js
@@ -31,12 +31,12 @@ User.signup = (req, res, next) => {
 
   User.login = (req, res, next) => {
     db.query(`SELECT * FROM users WHERE email = '${req.body.email}'`, function (err, res) {
-      console.log("res db", res);
+      console.log("res db", res.RowDataPacket);
       console.log("err db", err);
 ;      if (err) {
         next(err, null);
       } else {
-        bcrypt.compare(req.body.password, res.RowDataPacket.password)
+        bcrypt.compare(req.body.password, res.password)
           .then( valid => {
             console.log("valid", valid);
             if (!valid) {
